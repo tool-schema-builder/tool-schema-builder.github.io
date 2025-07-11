f70dd71a86682e583bd4f4d2e6e8a3fa0b46f404
sourceganesh <soccerganesh@gmail.com>
2025-07-11 20:08:34 +0530

    Changed SVG

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "tool-schema-builder.github.io",
   plugins: [react()],
   build: {
     outDir: 'build', // Output directory for the build
